c8f16a5c741c9ceababf74da073a07c5bb84c556
David Ingram <david@ingramaviation.com>
2019-08-16 18:19:03 +0000

    Automatically backed up by Learn

--- lib/third_challenge.rb
+++ lib/third_challenge.rb
@@ -1,20 +1,17 @@
-ef third_challenge
+def third_challenge
   epic_tragedy = {
    :montague => {
       :patriarch => {:name => "Lord Montague",
         :age => "53"
-        #your key/value pairs here
         },
       :matriarch => {
         :name => "Lady Montague",
         :age => "54"
-        #your key/value pairs here
         },
       :hero => {
         :name => "Romeo",
         :age => "15",
         :status => "alive"
-        #your key/value pairs here
         },
       :hero_friends => []
    }, 
@@ -22,24 +19,19 @@ ef third_challenge
       :patriarch => {
         :name => "Lord Capulet",
         :age => "50",
-        #your key/value pairs here
         },
       :matriarch => {
         :name => "Lady Capulet",
         :age => "51"
-        #your key/value pairs here
         },
       :heroine => {
         :name => "Juliet",
         :age => "15",
         :status => "alive"
-        #your key/value pairs here
         },
       :heroine_friends => []
    }
   }
-
-  
 end
 
 
